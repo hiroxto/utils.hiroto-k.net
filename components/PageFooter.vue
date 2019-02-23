d22d28e2ba783f893a869ab57ed08115cb1db2a2
Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
2019-02-23 16:02:49 +0900

    Create the page footer

---
+++ components/PageFooter.vue
@@ -0,0 +1,77 @@
+<template>
+  <footer class="footer">
+    <div class="container">
+      <div class="content">
+        <div class="columns">
+          <div class="column">
+            <p class="footer-column-title">
+              © 2019 hiroto-k
+            </p>
+          </div>
+
+          <div class="column">
+            <p class="footer-column-title">
+              Page links
+            </p>
+            <p
+              v-for="(pageLink, pageLinksKey) in pageLinks"
+              :key="pageLinksKey"
+              class="footer-column-item"
+            >
+              <nuxt-link
+                :to="pageLink.to"
+                :title="pageLink.title"
+                class="footer-column-link"
+              >
+                {{ pageLink.title }}
+              </nuxt-link>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+  export default {
+    name: 'PageFooter',
+    computed: {
+      pageLinks () {
+        return [
+          {
+            title: 'Top page',
+            to: '/',
+          },
+          {
+            title: 'Open urls',
+            to: '/open',
+          },
+        ];
+      },
+    },
+  };
+</script>
+
+<style scoped>
+  .footer-column-title {
+    color: #000000;
+    font-size: 1.2rem;
+    font-weight: 500;
+  }
+
+  .footer-column-item {
+    color: #000000;
+    font-size: 1rem;
+    font-weight: 400;
+    line-height: 1;
+  }
+
+  .footer-column-link {
+    color: #12428F;
+  }
+
+  .footer-column-link:hover {
+    color: #3273dc;
+  }
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="app">
     <nuxt></nuxt>
+    <page-footer></page-footer>
   </div>
 </template>
 
 <script>
+  import PageFooter from '~/components/PageFooter';
+
   export default {
+    components: {
+      PageFooter,
+    },
   };
 </script>
 
