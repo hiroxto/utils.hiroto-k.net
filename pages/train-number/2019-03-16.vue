9e967b32886c7d8bfc35c160282dafec84e70c60
Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
2019-03-14 22:12:02 +0900

    Create the 2019-03-16 page

---
+++ pages/train-number/2019-03-16.vue
@@ -0,0 +1,34 @@
+<template>
+  <mark-down-content :source="source">
+  </mark-down-content>
+</template>
+
+<script>
+  import MarkDownContent from '~/components/train-number/MarkDownContent';
+
+  export default {
+    components: {
+      MarkDownContent,
+    },
+    head () {
+      return {
+        title: this.pageTitle,
+      };
+    },
+    computed: {
+      pageTitle: () => '2019年3月16日 改正',
+      source () {
+        return `
+# ${this.pageTitle}
+
+現在作成中。
+        `;
+      },
+    },
+
+  };
+</script>
+
+<style scoped>
+
+</style>
