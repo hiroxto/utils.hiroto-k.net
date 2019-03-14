02cf1d10bb223421d59e8466dae5159439a9fa52
Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
2019-03-14 21:53:44 +0900

    Create the MarkDownContent component

---
+++ components/train-number/MarkDownContent.vue
@@ -0,0 +1,35 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <div class="content">
+        <vue-markdown :source="content">
+        </vue-markdown>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+  import VueMarkdown from 'vue-markdown';
+
+  export default {
+    name: 'MarkDownContent',
+    props: {
+      title: {
+        required: true,
+        type: String,
+      },
+      content: {
+        required: true,
+        type: String,
+      },
+    },
+    components: {
+      VueMarkdown,
+    },
+  };
+</script>
+
+<style scoped>
+
+</style>
