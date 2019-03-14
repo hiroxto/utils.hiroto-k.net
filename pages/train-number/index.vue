d9ed8a27bf2f8d1b33481a0f6e575e47048608bb
Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
2019-03-14 15:27:41 +0900

    Create the train number page

---
+++ pages/train-number/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <h1 class="title">
+        {{ pageTitle }}
+      </h1>
+
+      <aside class="menu">
+        <p class="menu-label">
+          Pages
+        </p>
+        <ul class="menu-list">
+          <li
+            v-for="(pageLink, pageLinksKey) in pageLinks"
+            :key="pageLinksKey"
+          >
+            <nuxt-link
+              :to="pageLink.to"
+              :title="pageLink.title"
+            >
+              {{ pageLink.title }}
+            </nuxt-link>
+          </li>
+        </ul>
+      </aside>
+    </div>
+  </section>
+</template>
+
+<script>
+  export default {
+    head () {
+      return {
+        title: this.pageTitle,
+      };
+    },
+    computed: {
+      pageTitle: () => '列車番号メモ',
+      pageLinks () {
+        return [
+        ];
+      },
+    },
+  };
+</script>
+
+<style scoped>
+
+</style>
