027ab2970548c962e349a68d3d45c0676e1eec38
Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
2019-03-15 14:17:02 +0900

    Add the subtitle to train number page index

--- pages/train-number/index.vue
+++ pages/train-number/index.vue
@@ -5,6 +5,10 @@
         {{ pageTitle }}
       </h1>
 
+      <h2 class="subtitle">
+        入出場,車輪転削などの予定臨や、パターンが概ね決まっている臨工や臨単の列車番号のメモ。
+      </h2>
+
       <aside class="menu">
         <p class="menu-label">
           Pages
