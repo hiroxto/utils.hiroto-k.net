dfed8a7abf62704de329802dc64145358081e170
Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
2019-03-15 23:45:18 +0900

    Create the pageLinks store

---
+++ store/pageLinks.js
@@ -0,0 +1,14 @@
+export const state = () => ({
+  topPageLink: {
+    title: 'Top page',
+    to: '/',
+  },
+  openUrlsPageLink: {
+    title: 'Open urls',
+    to: '/open',
+  },
+  trainNumberPageLink: {
+    title: 'Train number',
+    to: '/train-number',
+  },
+});
