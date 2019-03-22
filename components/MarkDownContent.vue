7f75d946eae916862a1e268ba83f0e0267e3e190
Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
2019-03-22 16:28:16 +0900

    Create the MarkDownContent component

---
+++ components/MarkDownContent.vue
@@ -0,0 +1,27 @@
+<template>
+  <div v-html="markedContent">
+  </div>
+</template>
+
+<script>
+  import marked from 'marked';
+
+  export default {
+    name: "MarkDownContent",
+    props: {
+      source: {
+        required: true,
+        type: String,
+      },
+    },
+    computed: {
+      markedContent() {
+        return marked(this.source);
+      },
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
