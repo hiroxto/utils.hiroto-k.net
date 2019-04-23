04150eeaaaa3be85a9a171d58b167d3c4e349f26
Hiroto Kitazawa <hiroto.ktzw@gmail.com>
2019-04-23 21:34:48 +0900

    Create the OpenButtons component

---
+++ components/pages/open/OpenButtons.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <div class="buttons">
+      <button
+        :class="buttonClass"
+        @click="openNormal"
+      >
+        Open normal
+      </button>
+      <button
+        :class="buttonClass"
+        @click="openReverse"
+      >
+        Open reverse
+      </button>
+    </div>
+
+    <div class="buttons">
+      <button
+        :class="buttonClass"
+        @click="openExceptNormal"
+      >
+        Open except normal
+      </button>
+      <button
+        :class="buttonClass"
+        @click="openExceptedReverse"
+      >
+        Open except reverse
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+  import { mapState, mapGetters } from 'vuex';
+
+  export default {
+    name: 'OpenButtons',
+    computed: {
+      ...mapState({
+        urls: state => state.openUrls.urls,
+      }),
+      ...mapGetters({
+        reversedUrls: 'openUrls/reversedUrls',
+        exceptedUrls: 'openUrls/exceptedUrls',
+        exceptedReversedUrls: 'openUrls/exceptedReversedUrls',
+      }),
+    },
+    methods: {
+      openNormal () {
+        this
+          .urls
+          .forEach(url => window.open(url.url, '_blank'));
+      },
+      openReverse () {
+        this
+          .reversedUrls
+          .forEach(url => window.open(url.url, '_blank'));
+      },
+      openExceptNormal () {
+        this
+          .exceptedUrls
+          .forEach(url => window.open(url.url, '_blank'));
+      },
+      openExceptedReverse () {
+        this
+          .exceptedReversedUrls
+          .forEach(url => window.open(url.url, '_blank'));
+      },
+    },
+  };
+</script>
+
+<style scoped>
+
+</style>
