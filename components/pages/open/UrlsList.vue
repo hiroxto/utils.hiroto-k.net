55fc57baabf51ab0a5234ba45aeaae483fd6e940
Hiroto Kitazawa <hiroto.ktzw@gmail.com>
2019-04-23 21:30:35 +0900

    Create the UrlsList component

---
+++ components/pages/open/UrlsList.vue
@@ -0,0 +1,39 @@
+<template>
+  <ul class="menu-list">
+    <li
+      v-for="(url, urlsKey) in urls"
+      :key="urlsKey"
+    >
+      <a
+        :href="url.url"
+        :title="buildUrlTitle(url)"
+        v-text="buildUrlTitle(url)"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+      </a>
+    </li>
+  </ul>
+</template>
+
+<script>
+  import {mapState} from 'vuex';
+
+  export default {
+    name: "UrlsList",
+    computed: {
+      ...mapState({
+        urls: state => state.openUrls.urls,
+      }),
+    },
+    methods: {
+      buildUrlTitle (url) {
+        return `${url.name} (${url.url})`;
+      },
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
