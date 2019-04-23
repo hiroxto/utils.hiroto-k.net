a64e9d0e3e2aae0523f327e76dd9f22a020925d2
Hiroto Kitazawa <hiroto.ktzw@gmail.com>
2019-04-23 21:10:43 +0900

    Create the InfoView component

---
+++ components/pages/open/InfoView.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <p>Total urls : {{ urlsCount }}</p>
+    <p>Excepted urls : {{ exceptedUrlsCount }}</p>
+    <p>Normal : {{ normalBrowsersText }}</p>
+    <p>Reverse : {{ reverseBrowsersText }}</p>
+  </div>
+</template>
+
+<script>
+  import { mapGetters } from 'vuex';
+
+  export default {
+    name: "InfoView",
+    computed: {
+      ...mapGetters({
+        urlsCount: 'openUrls/urlsCount',
+        exceptedUrlsCount: 'openUrls/exceptedUrlsCount',
+      }),
+      joinSeparator: () => ', ',
+      normalBrowsers () {
+        return [
+          'Mobile Firefox',
+        ];
+      },
+      normalBrowsersText () {
+        return this.normalBrowsers.join(this.joinSeparator);
+      },
+      reverseBrowsers () {
+        return [
+          'PC Firefox',
+          'PC Chrome',
+        ];
+      },
+      reverseBrowsersText () {
+        return this.reverseBrowsers.join(this.joinSeparator);
+      },
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
