7881c22e2eb33e262e8acd6a5d63bd3de1fff1ab
Hiroto Kitazawa <hiroto.ktzw@gmail.com>
2019-04-23 21:37:14 +0900

    Use the 'OpenButtons' component in open.vue file

--- pages/open.vue
+++ pages/open.vue
@@ -7,34 +7,8 @@
       <info-view>
       </info-view>
 
-      <div class="buttons">
-        <button
-          :class="buttonClass"
-          @click="openNormal"
-        >
-          Open normal
-        </button>
-        <button
-          :class="buttonClass"
-          @click="openReverse"
-        >
-          Open reverse
-        </button>
-      </div>
-      <div class="buttons">
-        <button
-          :class="buttonClass"
-          @click="openExceptNormal"
-        >
-          Open except normal
-        </button>
-        <button
-          :class="buttonClass"
-          @click="openExceptedReverse"
-        >
-          Open except reverse
-        </button>
-      </div>
+      <open-buttons>
+      </open-buttons>
 
       <urls-list>
       </urls-list>
@@ -44,8 +18,8 @@
 
 <script>
   import InfoView from '~/components/pages/open/InfoView';
+  import OpenButtons from "~/components/pages/open/OpenButtons";
   import UrlsList from '~/components/pages/open/UrlsList';
-  import { mapState, mapGetters } from 'vuex';
 
   export default {
     head () {
@@ -58,46 +32,12 @@
     },
     components: {
       InfoView,
+      OpenButtons,
       UrlsList,
     },
     computed: {
-      ...mapGetters({
-        reversedUrls: 'openUrls/reversedUrls',
-        exceptedUrls: 'openUrls/exceptedUrls',
-        exceptedReversedUrls: 'openUrls/exceptedReversedUrls',
-      }),
       title: () => 'Open urls',
       description: () => 'Open the urls.',
-      buttonClass: () => 'button is-medium is-danger is-rounded has-text-warning',
-    },
-    methods: {
-      openNormal () {
-        this
-          .urls
-          .forEach(url => window.open(url.url, '_blank'));
-      },
-      openReverse () {
-        this
-          .reversedUrls
-          .forEach(url => window.open(url.url, '_blank'));
-      },
-      openExceptNormal () {
-        this
-          .exceptedUrls
-          .forEach(url => window.open(url.url, '_blank'));
-      },
-      openExceptedReverse () {
-        this
-          .exceptedReversedUrls
-          .forEach(url => window.open(url.url, '_blank'));
-      },
     },
   };
 </script>
-
-<style scoped>
-  .buttons {
-    margin-top: 3vmin;
-    margin-bottom: 3vmin;
-  }
-</style>
