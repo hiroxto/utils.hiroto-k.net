35f48860eb4d2981ae69f79c9ba60d96ac9d93a8
Hiroto Kitazawa <hiroto.ktzw@gmail.com>
2019-05-17 19:21:44 +0900

    Create the LinksMenu component

---
+++ components/ui/LinksMenu.vue
@@ -0,0 +1,38 @@
+<template>
+  <aside class="menu">
+    <p class="menu-label" v-text="label">
+    </p>
+
+    <ul class="menu-list">
+      <li
+        v-for="(link, linksKey) in links"
+        :key="linksKey"
+      >
+        <nuxt-link
+          :to="link.to"
+          :title="link.title"
+          v-text="link.title"
+        >
+        </nuxt-link>
+      </li>
+    </ul>
+  </aside>
+</template>
+
+<script>
+  export default {
+    name: 'LinksMenu',
+    props: {
+      links: {
+        required: true,
+        type: Array,
+      },
+      label: {
+        type: String,
+        default () {
+          return 'pages';
+        },
+      },
+    },
+  };
+</script>
