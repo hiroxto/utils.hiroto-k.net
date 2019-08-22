bc1b60adfb94adf6d6b66db8724c3adbeace710e
Hiroto Kitazawa <hiroto.ktzw@gmail.com>
2019-08-22 21:13:34 +0900

    Fix the domains

--- pages/index.vue
+++ pages/index.vue
@@ -36,7 +36,7 @@ export default Vue.extend({
       return this.$store.getters['pageLinks/pageLinks'];
     },
     title (): string {
-      return 'utils.hiroto-k.net';
+      return 'utils.hiroto-k.net￿';
     },
     description (): string {
       return 'Utility site for me.';
