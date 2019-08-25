7771111fcc2fbcc9d7f0cc9a359e9bf57938b3ea
Hiroto Kitazawa <hiroto.ktzw@gmail.com>
2019-08-25 19:52:01 +0900

    Create the train number calc page

---
+++ pages/train-number-calc/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <h1 class="title" v-text="title">
+      </h1>
+
+      <h2 class="subtitle" v-text="description">
+      </h2>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  head () {
+    return {
+      title: this.title,
+      meta: [
+        { hid: 'description', name: 'description', content: this.description },
+      ],
+    };
+  },
+  components: {
+  },
+  computed: {
+    title (): string {
+      return '列車番号から列車種別を計算';
+    },
+    description (): string {
+      return '列車番号から列車種別(特急客, 臨急客, 臨特急客, 高速貨A, 臨専貨A, など)を計算できるページ。';
+    },
+  },
+});
+</script>
