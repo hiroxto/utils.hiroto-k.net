79482f06efe2ddf44922762c480d8fb234f48755
Hiroto Kitazawa <hiroto.ktzw@gmail.com>
2019-09-02 16:08:03 +0900

    Add transpile property to nuxt configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -59,6 +59,10 @@ const nuxtConfig: NuxtConfiguration = {
   build: {
     extractCSS: true,
 
+    transpile: [
+      'vee-validate/dist/rules',
+    ],
+
     extend (config, { isDev, isClient }): void {
       config.module.rules.push({
         test: /\.md$/,
