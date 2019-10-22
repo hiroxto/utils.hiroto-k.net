77862139f4594965631e5345d7f024edc84165d2
Hiroto Kitazawa <hiroto.ktzw@gmail.com>
2019-10-22 15:43:14 +0900

    Implement the TrainNumberCalc class constructor

--- lib/TrainNumberCalc.ts
+++ lib/TrainNumberCalc.ts
@@ -1,4 +1,17 @@
+/**
+ * 列車番号から列車種別を計算するクラス
+ */
 class TrainNumberCalc {
+  protected trainNumber: string;
+
+  /**
+   * TrainNumberCalcクラスのコンストラクタ
+   *
+   * @param trainNumber 計算する列車番号
+   */
+  constructor (trainNumber: string) {
+    this.trainNumber = trainNumber;
+  }
 }
 
 export default TrainNumberCalc;
