77548277f5a7a639006b10eafd7944d97f1cf216
Hiroto Kitazawa <hiroto.ktzw@gmail.com>
2019-10-22 16:49:18 +0900

    Implement the isPassengerNumber, isFreightNumber, isPassengerSpecial and isFreightSpecial methods

--- lib/TrainNumberCalc.ts
+++ lib/TrainNumberCalc.ts
@@ -27,6 +27,42 @@ class TrainNumberCalc {
   }
 
   /**
+   * 旅客列車かの判別
+   *
+   * 列車番号の十位が4以下(下2ケタが00~49)の場合は旅客列車
+   */
+  isPassengerNumber (): boolean {
+    return this.splitNumber[2] <= 4;
+  }
+
+  /**
+   * 旅客列車かの判別
+   *
+   * 列車番号の十位が5以上(下2ケタが50~99)の場合は旅客列車
+   */
+  isFreightNumber (): boolean {
+    return this.splitNumber[2] >= 5;
+  }
+
+  /**
+   * 旅客列車の臨時列車かの判別
+   *
+   * 列車番号の千位が6以上の場合は臨時列車
+   */
+  isPassengerSpecial (): boolean {
+    return this.splitNumber[0] >= 6;
+  }
+
+  /**
+   * 貨物列車の臨時列車かの判別
+   *
+   * 列車番号の千位が8以上の場合は臨時列車
+   */
+  isFreightSpecial (): boolean {
+    return this.splitNumber[0] >= 8;
+  }
+
+  /**
    * 列車番号を1文字ずつに分割したsplitNumberを設定
    */
   private setSplitNumber (): void {
