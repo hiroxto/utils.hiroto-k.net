21d0e41108c99fbbe71a5f1a5128d825fa7fb322
Hiroto Kitazawa <hiroto.ktzw@gmail.com>
2019-11-11 16:21:29 +0900

    Create RenderAsField component

---
+++ components/pages/qr-code/RenderAsField.vue
@@ -0,0 +1,43 @@
+<template>
+  <b-field>
+    <div class="block">
+      <label class="label">
+        レンダリング方式
+      </label>
+
+      <b-radio
+        v-model="renderAs"
+        name="name"
+        native-value="canvas"
+      >
+        canvas
+      </b-radio>
+
+      <b-radio
+        v-model="renderAs"
+        name="name"
+        native-value="svg"
+      >
+        svg
+      </b-radio>
+    </div>
+  </b-field>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'RenderAsField',
+  computed: {
+    renderAs: {
+      get (): string {
+        return this.$store.state.qrCodeGenerator.renderAs;
+      },
+      set (renderAs): void {
+        this.$store.commit('qrCodeGenerator/setRenderAs', renderAs);
+      },
+    },
+  },
+});
+</script>
