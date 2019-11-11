214586ca39e5dbb264722f587a2a5e769db81b47
Hiroto Kitazawa <hiroto.ktzw@gmail.com>
2019-11-11 16:00:21 +0900

    Create QrCodeRenderAsOptionValue type

--- types/index.d.ts
+++ types/index.d.ts
@@ -10,6 +10,7 @@ export interface OpenLink extends Link {
 }
 
 export type QrCodeErrorCorrectionLevel = 'L' | 'M' | 'Q' | 'H';
+export type QrCodeRenderAsOptionValue = 'canvas' | 'svg';
 
 export type TrainNumberType = '特急客' | '臨特急客' | '急客' | '臨急客' | '客' | '臨客' |
                               '高速貨A' | '臨高速貨A' | '高速貨B' | '臨高速貨B' | '高速貨C' | '臨高速貨C' |
