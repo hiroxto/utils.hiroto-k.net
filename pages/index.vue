6c77ae0c888aecd0a71fdf052f60e137993a676d
hiroxto <hiroxto@gmail.com>
2021-07-11 17:46:41 +0900

    ページ内のドメインや名前を変更

--- pages/index.vue
+++ pages/index.vue
@@ -36,7 +36,7 @@ export default Vue.extend({
       return this.$store.getters['pageLinks/pageLinks'];
     },
     title (): string {
-      return 'utils.hiroto-k.net';
+      return 'utils.hiroxto.net';
     },
     description (): string {
       return 'Utility site for me.';
