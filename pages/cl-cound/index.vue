968f86993110214ad7ef45a9a87b9b2d1f3bbfe0
hiroxto <hiroxto@gmail.com>
2021-07-12 18:19:52 +0900

    コンタクトレスのサウンドを鳴らすページを作成

---
+++ pages/cl-cound/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <h1 class="title" v-text="title">
+      </h1>
+
+      <h2 class="subtitle" v-text="description">
+      </h2>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  head () {
+    return {
+      title: this.title,
+      meta: [
+        { hid: 'description', name: 'description', content: this.description },
+      ],
+    };
+  },
+  computed: {
+    title (): string {
+      return 'EMVコンタクトレスのサウンドをWeb Audio APIで再生';
+    },
+    description (): string {
+      return 'EMVコンタクトレスのサウンドをWeb Audio APIで再生';
+    },
+  },
+});
+</script>
