ba5aaae460f312f6b5b7939363cd75c0b9213409
hiroxto <hiroxto@gmail.com>
2021-07-12 20:00:39 +0900

    仕様を書くコンポーネントを作成

---
+++ components/pages/cl-sound/Specification.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="specification">
+    <h3>仕様</h3>
+    <div class="contents">
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'Specification',
+});
+</script>
